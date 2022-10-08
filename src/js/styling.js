4d93bf1b94c454bb12c5954a31db03a01ab2a9ea
SmylerMC <smyler@mail.com>
2022-10-08 23:03:45 +0200

    Mobile friendly menu

--- src/js/styling.js
+++ src/js/styling.js
@@ -1,4 +1,5 @@
 const header = document.querySelector("header");
+const nav = header.querySelector("nav");
 
 if (
     "IntersectionObserver" in window &&
@@ -6,15 +7,21 @@ if (
     "intersectionRatio" in window.IntersectionObserverEntry.prototype
 ) {
     let observer = new IntersectionObserver(entries => {
-        console.log(entries);
         if (!entries[0].isIntersecting) {
             header.setAttribute("data-collapsed", "yes");
         }
     });
     observer.observe(header.querySelector(".blue-stripe"));
-    addEventListener('scroll', event => {
+    addEventListener('scroll', _ => {
         if (window.scrollY <= 0) {
             header.setAttribute("data-collapsed", "no");
         }
     });
 }
+
+const toggleMobileMenu = () => {
+    let current = nav.getAttribute("data-collapsed");
+    nav.setAttribute("data-collapsed",
+        current === "no" ? "yes": "no"
+    );
+};
