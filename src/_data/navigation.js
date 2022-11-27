7b812fea4d3d4d81b37334b8fe8ea3c7c1dcd293
SmylerMC <smyler@mail.com>
2022-11-27 19:01:30 +0100

    Fix navigation links

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -10,11 +10,11 @@ class NavigationEntry {
 }
 
 module.exports = [
-  new NavigationEntry('index', 'navigation.home', true),
-  new NavigationEntry('maps', 'navigation.maps', true),
-  new NavigationEntry('gallery', 'navigation.gallery', true),
-  new NavigationEntry('rules', 'navigation.rules', true),
-  new NavigationEntry('rules#ranks', 'navigation.ranks', false),
-  new NavigationEntry('faq', 'navigation.faq', true),
+  new NavigationEntry('../', 'navigation.home', true),
+  new NavigationEntry('../maps/', 'navigation.maps', true),
+  new NavigationEntry('../gallery/', 'navigation.gallery', true),
+  new NavigationEntry('../rules/', 'navigation.rules', true),
+  new NavigationEntry('../rules/#ranks', 'navigation.ranks', false),
+  new NavigationEntry('../faq/', 'navigation.faq', true),
   new NavigationEntry('https://remi.btefrance.fr', 'navigation.remi', false),
 ];
