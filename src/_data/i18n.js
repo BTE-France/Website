240f3a6ad8e86c3d94ca7575a843061f780ea707
SmylerMC <smyler@mail.com>
2022-11-27 13:26:55 +0100

    404 page templating

--- src/_data/i18n.js
+++ src/_data/i18n.js
@@ -1,2 +1,12 @@
 module.exports = {
-}
+  notFound: {
+    title: {
+      fr: 'Erreur 404',
+      en: 'Error 404'
+    },
+    message: {
+      fr: "Êtes-vous perdus ? On dirait bien que cette page n'existe pas, peut-être n'avons nous pas encore eu le temps de la construire.",
+      en: "Are you lost ? This page does not exist, maybe we haven\'t had time to build it yet."
+    }
+  }
+};
