b1049ed627e839cc663400764e277fd4fe47bbb9
SmylerMC <smyler@mail.com>
2022-11-27 13:55:30 +0100

    Header translation

--- src/_data/i18n.js
+++ src/_data/i18n.js
@@ -1,4 +1,52 @@
 module.exports = {
+  header: {
+    h1: {
+      fr: "BTE France",
+      en: "BTE France"
+    },
+    h2: {
+      fr: "Nous construisons la France sur Minecraft, à échelle réelle.",
+      en: "We are building France in Minecraft, one to one scale."
+    },
+    bannerAlt: {
+      fr: "Logo de BTE France",
+      en: "BTE France logo"
+    },
+    burgerAlt: {
+      fr: "Menu principal",
+      en: "Main menu"
+    },
+    nav: {
+      home: {
+        fr: "Accueil",
+        en: "Home"
+      },
+      maps: {
+        fr: "Cartes",
+        en: "Maps"
+      },
+      gallery: {
+        fr: "Galerie",
+        en: "Gallery"
+      },
+      rules: {
+        fr: "Règles",
+        en: "Rules"
+      },
+      faq: {
+        fr: "FAQ",
+        en: "FAQ"
+      },
+      languageAlt: {
+        fr: "Menu des langues",
+        en: "Language menu"
+      },
+      darkModeAlt: {
+        fr: "Mode sombre",
+        en: "Dark mode"
+      }
+    }
+  },
   notFound: {
     title: {
       fr: 'Erreur 404',
