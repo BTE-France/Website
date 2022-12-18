4e6a4e9660722b19f9ca4d0d3e51fcf5bb8048bc
SmylerMC <smyler@mail.com>
2022-12-18 11:10:33 +0100

    Navigation link localization

--- .eleventy.js
+++ .eleventy.js
@@ -15,11 +15,27 @@ const desaccentify = s => {
             .replaceAll(/Ç/g, 'C');
 }
 
+const localizeUrl = (path, locale) => {
+    if (/^[a-z]+:\/\//.test(path)) { // Starts with a protocole schema
+        return path;
+    }
+    if (/^\/[a-z]{2}\//.test(path)) { // Already has a two letter language code
+        return path;
+    }
+    if (path.startsWith('/')) {
+        path = '/' + locale + path;
+    } else  {
+        console.warn("Found a relative URL when localizing, this is unsafe. Will not localize '" + path + "'");
+    }
+    return path;
+};
+
 module.exports = function(eleventyConfig) {
     let nunjucksEnvironment = new Nunjucks.Environment(
         new Nunjucks.FileSystemLoader("src/_includes")
     );
 
+    eleventyConfig.addShortcode('url', localizeUrl)
     eleventyConfig.addFilter('desaccentify', desaccentify);
 
     eleventyConfig.addPassthroughCopy("src/assets");
