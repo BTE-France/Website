80e7e3635f35eaeb8bddd053daccd2121e09f3c7
SmylerMC <smyler@mail.com>
2022-12-21 16:11:32 +0100

    Move rules to a data file

---
+++ src/_data/rules.js
@@ -0,0 +1,66 @@
+module.exports = {
+  'discord': [
+    {
+      'en': "**All forms of racism, sexism, homophobia, and other forms of prejudice are prohibited.** Includes voice, Text channels, DMs with no exceptions."
+    },
+    {
+      'en': "**Sending any attachment (video/image/link) that contains erotic/sexual content or gore is forbidden.** Suggestive/Minor NSFW in text, voice, emojis and reactions is also not allowed, but is taken more lightly."
+    },
+    {
+      'en': "**Talk of politics and other heavy topics is prohibited in all channels.** This is a server dedicated to the Build The Earth project alone, and we do not allow heavy or dividing topics to be discussed."
+    },
+    {
+      'en': "**All aspects of Discord TOS must be followed (including but not limited to the age requirement of 13).** Jokes about breaking TOS will be treated as facts. Read up [here](https://discord.com/terms)."
+    },
+    {
+      'en': "**You are not allowed to play on a cracked account (an illegitimate Minecraft account).** This does not include launchers like MultiMc or Badlion as you require a legitimate Minecraft account to use them."
+    },
+    {
+      'en': "**Criminal behaviour (death threats, blackmailing, etc...) is not allowed.** If the context is shown to disprove actual criminal behaviour, it will be met by a warning for further discretion."
+    },
+    {
+      'en': "**Having a username/profile picture/status/Game Activity that contains any violation of our rules is prohibited.** This also includes impersonation of Staff members (Having an identical profile picture and username/nickname)."
+    },
+    {
+      'en': "**Unsolicited advertising (through voice, text channels, or DMs) is prohibited.**"
+    },
+    {
+      'en': "**Accusing someone for breaking rules in public chats is prohibited.** Instances of such are considered defamatory. If you have any proof, contact a Staff member privately about it and we will handle the situation accordingly."
+    },
+    {
+      'en': "**General toxic behaviour is not allowed in any channels.** This includes, but is not limited to, earrape, loud/annoying noises and screams in voice chats and posted audio/video files, spreading slander/lies, and rude, insulting or derogatory language towards users/staff."
+    },
+    {
+      'en': "**Usage of channels that deviate from their purpose is not allowed.** Please read the channel description before chatting to avoid punishment by Staff."
+    },
+    {
+      'en': "**Evading any punishment is heavily forbidden.**"
+    },
+    {
+      'en': "**Exploiting rule loopholes is also not allowed.** General common sense applies to all rules, and must be reported immediately."
+    }
+  ],
+  'minecraft': [
+    {
+      'en': "**Rules from the Discord server also apply on the Minecraft server.**"
+    },
+    {
+      'en': "**Grieffing is strictly prohibited and will result in a ban.** Is considered grieffing **ANY** modification to a construction without the explicit permission of the original builder or of a staff member."
+    },
+    {
+      'en': "**Useless terrain generation is forbidden.** This includes generating chunks outside of France or in areas where you do not intend to build."
+    },
+    {
+      'en': "**Constructions that do not follow the Build The Earth construction guidelines are forbidden. Such and constructions will be deleted. Repeated infractions may result in a banishment.**"
+    },
+    {
+      'en': "**Redstone systems and lag machines are strictly prohibited**. Constructing any will result in a ban."
+    },
+    {
+      'en': "**Abusing WorldEdit with large selections or any intensive workloads is forbidden.**"
+    },
+    {
+      'en': "**Crashing the server on purpose, or attempting to do so, will be sanctioned with a permanent banishment.**"
+    }
+  ]
+}
